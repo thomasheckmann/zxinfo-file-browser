752015f5889a326f860b78dd8423fe4c3658db8d
Thomas Heckmann <thomas.kolbeck@gmail.com>
2023-05-31 22:09:40 +0200

    clean up - customZXDBid converted to simple value

--- src/common/cardactions/ZXdbID.jsx
+++ src/common/cardactions/ZXdbID.jsx
@@ -9,13 +9,13 @@
 import { IconButton, InputAdornment, Snackbar, TextField } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
 import { mylog } from "../../App";
-import {ZXInfoSettingsCtx} from "../ZXInfoSettings";
+import { ZXInfoSettingsCtx } from "../ZXInfoSettings";
 import LaunchTwoToneIcon from "@mui/icons-material/LaunchTwoTone";
 
-export default function ZXdbID(props) {
-  const [customZXDBid, setCustomZXDBid] = useState({ value: "" });
+export default function ZXdbID({ entry }) {
+  const [customZXDBid, setCustomZXDBid] = useState(null);
   const [appSettings, setAppSettings] = useContext(ZXInfoSettingsCtx);
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleClick = () => {
     setOpen(true);
@@ -38,49 +38,49 @@ export default function ZXdbID(props) {
       if (map.size === 0) {
         return;
       }
-      const mapEntry = map.get(props.entry.sha512);
+      const mapEntry = map.get(entry.sha512);
       if (mapEntry) {
         // remove filepath from array
         mylog("ZXdbID", "handleZXdbID", `empty, remove from array...`);
         const newArr = mapEntry.files.filter((object) => {
-          return object !== props.entry.filepath;
+          return object !== entry.filepath;
         });
 
         if (newArr.length === 0) {
           mylog("ZXdbID", "handleZXdbID", `empty, no more files in array, remove from map`);
-          map.delete(props.entry.sha512);
+          map.delete(entry.sha512);
         } else {
-          map.set(props.entry.sha512, { zxdbid: mapEntry.zxdbid, files: newArr });
+          map.set(entry.sha512, { zxdbid: mapEntry.zxdbid, files: newArr });
         }
       }
     } else if (map.size === 0) {
       // first time...
       mylog("ZXdbID", "handleZXdbID", `first time, creating the map`);
-      mylog("ZXdbID", "handleZXdbID", `hash: ${props.entry.sha512}, file: ${props.entry.filepath}`);
+      mylog("ZXdbID", "handleZXdbID", `hash: ${entry.sha512}, file: ${entry.filepath}`);
       map = new Map();
-      map.set(props.entry.sha512, { zxdbid: event.target.value, files: [props.entry.filepath] });
+      map.set(entry.sha512, { zxdbid: event.target.value, files: [entry.filepath] });
     } else {
-      const mapEntry = map.get(props.entry.sha512);
+      const mapEntry = map.get(entry.sha512);
       if (mapEntry) {
         // zxdb id entry exists, check if its a new filename
         mylog("ZXdbID", "handleZXdbID", `${mapEntry.zxdbid} - exists in map...`);
         const files = mapEntry.files;
-        if (files.includes(props.entry.filepath)) {
-          mylog("ZXdbID", "handleZXdbID", `${props.entry.filepath} - exists in list, updating `);
-          map.set(props.entry.sha512, { zxdbid: event.target.value, files: files });
+        if (files.includes(entry.filepath)) {
+          mylog("ZXdbID", "handleZXdbID", `${entry.filepath} - exists in list, updating `);
+          map.set(entry.sha512, { zxdbid: event.target.value, files: files });
         } else {
-          mylog("ZXdbID", "handleZXdbID", `${props.entry.filepath} - new, add to list`);
-          map.set(props.entry.sha512, { zxdbid: event.target.value, files: [...files, props.entry.filepath] });
+          mylog("ZXdbID", "handleZXdbID", `${entry.filepath} - new, add to list`);
+          map.set(entry.sha512, { zxdbid: event.target.value, files: [...files, entry.filepath] });
         }
       } else {
         // new (zxdb id) entry
         mylog("ZXdbID", "handleZXdbID", `${event.target.value} - new entry to map`);
-        map.set(props.entry.sha512, { zxdbid: event.target.value, files: [props.entry.filepath] });
+        map.set(entry.sha512, { zxdbid: event.target.value, files: [entry.filepath] });
       }
     }
 
     setAppSettings((appSettings) => ({ ...appSettings, zxdbIDs: map }));
-    setCustomZXDBid({ value: event.target.value });
+    setCustomZXDBid(event.target.value);
     var obj = Object.fromEntries(map);
     var jsonString = JSON.stringify(obj);
     window.electronAPI.setZXDBs("zxdb-id-store", jsonString);
@@ -88,36 +88,37 @@ export default function ZXdbID(props) {
   };
 
   const handleInputChange = (event) => {
-    setCustomZXDBid({ value: event.target.value });
+    setCustomZXDBid(event.target.value);
   };
 
   const openLink = () => {
-    window.electronAPI.openZXINFODetail(customZXDBid.value).then((res) => {});
+    window.electronAPI.openZXINFODetail(customZXDBid).then((res) => {});
   };
 
   useEffect(() => {
-    mylog("ZXdbID", "useEffect", `init: ${props.entry.sha512}`);
-    const customId = appSettings.zxdbIDs.get(props.entry.sha512);
+    mylog("ZXdbID", "useEffect", `init: ${entry.sha512}`);
+    const customId = appSettings.zxdbIDs.get(entry.sha512);
     if (customId) {
-      setCustomZXDBid({ value: customId.zxdbid });
+      setCustomZXDBid(customId.zxdbid);
       mylog("ZXdbID", "useEffect", `found ZXDB ID: ${customId.zxdbid}`);
     }
-  }, [props.entry.sha512, appSettings.zxdbIDs]);
+  }, [entry.sha512, appSettings.zxdbIDs]);
 
   return (
     <React.Fragment>
-      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose} message={`ZXDB ID - ${customZXDBid.value} saved`} />
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose} message={`ZXDB ID - ${customZXDBid} saved`} />
       <TextField
         label="ZXDB ID"
         id="outlined-size-small"
         size="small"
-        value={customZXDBid.value}
+        value={customZXDBid}
         type="number"
         InputProps={{
-          endAdornment: customZXDBid.value && (
+          endAdornment: customZXDBid && (
             <InputAdornment position="end">
               <IconButton aria-label="More details at ZXInfo.dk" onClick={openLink} edge="end">
-              <LaunchTwoToneIcon /></IconButton>
+                <LaunchTwoToneIcon />
+              </IconButton>
             </InputAdornment>
           ),
         }}
